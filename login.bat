751a0e60dc8910c7446ec2bb8ff295ca13031c62
vinodrajyn <58675759+vinodrajyn@users.noreply.github.com>
2019-12-17 15:10:38 +0530

    need to review

--- login.bat
+++ login.bat
@@ -1,1 +1,2 @@
 echo "This is a Login code"
+echo "Username and Password Code"
--- login.java
+++ login.java
@@ -4,5 +4,6 @@ public class login {
     System.out.println("By Entering the credential");
 	System.out.println("And click on homepage");
 	System.out.println("Find all the modules");
+	System.out.println("Finally review");
   }
 }
