4f184524660868c2c1bf2d3ad31da4353770075c
vinodrajyn <58675759+vinodrajyn@users.noreply.github.com>
2019-12-17 14:57:08 +0530

    Update login.java

--- login.java
+++ login.java
@@ -1,6 +1,7 @@
 public class login {
   public static void main(String[] args) {
     System.out.println("Hello, Kindly login to the application");
-    System.out.println("By Enter the credential");
+    System.out.println("By Entering the credential");
+	System.out.println("And click on homepage");
   }
 }
